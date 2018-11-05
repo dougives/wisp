5dcb60f2e31dc6abc7bc32ae687b101068cd537a
Doug Ives <github@dou.gives>
2018-11-05 16:11:15 +0000

    single mon

--- dream.c
+++ dream.c
@@ -3,21 +3,16 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdint.h>
+#include <signal.h>
 
 #include <pcap/pcap.h>
 
 #define SNAPLEN 262144
 
-struct DeviceArgs
-{
-    size_t count;
-    char *names[];
-};
-
 struct Args
 {
-    struct DeviceArgs *monitors;
-    struct DeviceArgs *injectors;
+    const char *monitor;
+    const char *fields;
 };
 
 struct ieee80211_radiotap_header {
@@ -42,123 +37,102 @@ struct ieee80211_frame_header_stub {
     };
 } __attribute__((__packed__));
 
+struct ieee80211_info
+{
+    char *bss;
+    char *sta;
+    uint16_t freq;
+};
+
 static char pcaperr[PCAP_ERRBUF_SIZE];
 static struct Args args;
-static pcap_if_t *iflist;
+static pcap_if_t *iflist = NULL;
+static pcap_t *live = NULL;
 
+// static void parse_device_args(char *arg, struct DeviceArgs *devargs)
+// {
+//     size_t len = strlen(arg);
+//     if (len == 0)
+//         return;
+//     for (int j = 0, h = 0; 
+//         j <= len && h < len; 
+//         ++j)
+//     {
+//         switch (arg[j])
+//         {
+//             case ',':
+//             case '\0':
+//                 assert((devargs = (struct DeviceArgs *)
+//                     realloc(devargs, 
+//                         sizeof(struct DeviceArgs) 
+//                         * (devargs->count + 1))));
+//                 devargs->names[devargs->count++] = &arg[h];
+//                 arg[j] = '\0';
+//                 h = j + 1;
+//         }
+//     }
+// }
 
-static void parse_device_args(char *arg, struct DeviceArgs *devargs)
-{
-    size_t len = strlen(arg);
-    if (len == 0)
-        return;
-    for (int j = 0, h = 0; 
-        j <= len && h < len; 
-        ++j)
-    {
-        switch (arg[j])
-        {
-            case ',':
-            case '\0':
-                assert((devargs = (struct DeviceArgs *)
-                    realloc(devargs, 
-                        sizeof(struct DeviceArgs) 
-                        * (devargs->count + 1))));
-                devargs->names[devargs->count++] = &arg[h];
-                arg[j] = '\0';
-                h = j + 1;
-        }
-    }
-}
-
-static void verify_device_names(void)
+static void verify_device_name(void)
 {
     assert(!pcap_findalldevs(&iflist, pcaperr));
-    size_t devcount = 
-        args.monitors->count 
-        + args.injectors->count;
-    char *devs[devcount];
-    for (int i = 0; i < devcount; ++i)
+    int has_device = 0;
+    for (
+        pcap_if_t *ifnode = iflist; 
+        ifnode; 
+        ifnode = ifnode->next)
     {
-        devs[i] =
-            i < args.monitors->count
-                ? args.monitors->names[i]
-                : args.injectors->names[i-args.monitors->count];
-        //printf("dev: %s\n", devs[i]);
-        int has_device = 0;
-        for (
-            pcap_if_t *ifnode = iflist; 
-            ifnode; 
-            ifnode = ifnode->next)
+        //printf("ifn: %s\n", ifnode->name);
+        if ((has_device |= !strncmp(args.monitor, ifnode->name, 32)))
         {
-            //printf("ifn: %s\n", ifnode->name);
-            if ((has_device |= !strncmp(devs[i], ifnode->name, 32)))
-            {
-                break;
-            }
+            break;
         }
-        assert(has_device);
     }
+    assert(has_device);
     pcap_freealldevs(iflist);
 }
 
 static void verify_monitor_mode(void)
 {
-    for (int i = 0; i < args.monitors->count; ++i)
+    pcap_t *pcap = pcap_create(args.monitor, pcaperr);
+    pcap_activate(pcap);
+    int *dlts = NULL;
+    int dltcount = pcap_list_datalinks(pcap, &dlts);
+    int has_monitor_mode = 0;
+    for (
+        int *dlt = dlts; 
+        dltcount > 0; 
+        --dltcount, dlt += sizeof(*dlt))
     {
-        pcap_t *pcap = pcap_create(args.monitors->names[i], pcaperr);
-        pcap_activate(pcap);
-        int *dlts = NULL;
-        int dltcount = pcap_list_datalinks(pcap, &dlts);
-        int has_monitor_mode = 0;
-        for (
-            int *dlt = dlts; 
-            dltcount > 0; 
-            --dltcount, dlt += sizeof(*dlt))
+        if (has_monitor_mode |= *dlt == DLT_IEEE802_11_RADIO)
         {
-            if (has_monitor_mode |= *dlt == DLT_IEEE802_11_RADIO)
-            {
-                break;
-            }
+            break;
         }
-        pcap_close(pcap);
-        
-        assert(has_monitor_mode);
     }
+    pcap_close(pcap);
+    assert(has_monitor_mode);
 }
 
 static void print_device_args(void)
 {
-    printf("monitors[%ld]:", args.monitors->count);
-    for (int i = 0; i < args.monitors->count; ++i)
-        printf(" %s", args.monitors->names[i]);
-    printf("\ninjectors[%ld]:", args.injectors->count);
-    for (int i = 0; i < args.injectors->count; ++i)
-        printf(" %s", args.injectors->names[i]);
-    printf("\n");
+    printf("monitor: %s\n", args.monitor);
 }
 
 static void parse_args(int argc, char **argv)
 {
     assert(argc > 1);
-    assert((args.monitors = (struct DeviceArgs *)
-        calloc(1, sizeof(struct DeviceArgs))));
-    assert((args.injectors = (struct DeviceArgs *)
-        calloc(1, sizeof(struct DeviceArgs))));
+    args.fields = "cfbs";
     for (int i = 1; i < argc; ++i)
     {
         switch (*argv[i])
         {
-            case 'm':
-                assert(argv[i][1] == '=');
-                parse_device_args(&argv[i][2], args.monitors);
-                break;
-            case 'i':
-                assert(argv[i][1] == '=');
-                parse_device_args(&argv[i][2], args.injectors);
-                break;
+            case '-':
+                assert(argv[i][1]);
+                args.fields = argv[i] + 1;
             default:
-                assert(0);
+                args.monitor = argv[i];
+                // parse_device_args(argv[i], args.monitor);
+                break;
         }
     }
 }
@@ -173,6 +147,37 @@ int freqtochan(int freq)
     assert(freq >= 2412 && freq <= 2484);
     return freq == 2484 ? 14
         : 1 + (freq - 2412) / 5;
+}
+
+void print_bytes_line(const char *bytes, size_t n)
+{
+    for (int i = 0; i < n; ++i)
+        printf("%02x", bytes[n]);
+    putchar('\n');
+}
+
+void dump_fields(struct ieee80211_info info)
+{
+    for (int j = 1; j < strlen(args.fields) - 1; ++j)
+    {
+        switch (args.fields[j])
+        {
+            case 'b':
+                print_bytes_line(info.bss, sizeof(info.bss));
+                continue;
+            case 'c':
+                printf("%d\n", freqtochan(info.freq));
+                continue;
+            case 'f':
+                printf("%d\n", info.freq);
+                continue;
+            case 's':
+                print_bytes_line(info.sta, sizeof(info.sta));
+                continue;
+            default:
+                assert(0);
+        }
+    }
 }
 
 void got_packet(
@@ -196,7 +201,7 @@ void got_packet(
     // for (int i = 0; i < rtap->it_len; ++i)
     //     printf("%02x", packet[i]);
     // printf("\n");
-    uint8_t bssid[6], stmac[6];
+    char bssid[6], stmac[6];
     memset(bssid, 0, 6);
     memset(stmac, 0, 6);
     switch (h80211->data[1] & 3)
@@ -223,33 +228,53 @@ void got_packet(
     }
     assert(rtap->it_present&8);
     uint16_t freq = *(uint16_t *)(packet + 10);
-    printf("freq: %d\nbss: ", freq);
-    for (int i = 0; i < 6; ++i)
-        printf("%02x", bssid[i]);
-    printf("\nsta: ");
-    for (int i = 0; i < 6; ++i)
-        printf("%02x", stmac[i]);
-    printf("\n----------------\n");
+    // printf("chan: %d\n", freqtochan(freq));
+    // printf("freq: %d\nbss: ", freq);
+    // for (int i = 0; i < 6; ++i)
+    //     printf("%02x", bssid[i]);
+    // printf("\nsta: ");
+    // for (int i = 0; i < 6; ++i)
+    //     printf("%02x", stmac[i]);
+    // printf("\n----------------\n");
     //assert(freq == 2412);
     //pcap_breakloop((pcap_t *)args);
+    dump_fields((struct ieee80211_info)
+        {
+            .bss = bssid,
+            .freq = freq,
+            .sta = stmac,
+        });
 }
 
-void listen_for_associations(void)
+void shutdown(int code)
 {
-    assert(args.monitors->count > 0);
-    pcap_t *live[args.monitors->count];
-    char errbuf[PCAP_ERRBUF_SIZE];
-    for (int i = 0; i < args.monitors->count; ++i)
+    pcap_close(live);
+    exit(code);
+}
+
+void listen(void)
+{
+    assert(args.monitor);
+    live = pcap_open_live(
+        args.monitor, 
+        SNAPLEN, 1, 0, pcaperr);
+    assert(live);
+    pcap_loop(live, -1, &got_packet, (unsigned char *)live);
+}
+
+void signal_handler(int sig)
+{
+    switch (sig)
     {
-        live[i] = pcap_open_live(
-            args.monitors->names[i], 
-            SNAPLEN, 1, 0, errbuf);
-        assert(live[i]);
-    }
-    pcap_loop(live[0], -1, &got_packet, (unsigned char *)live[0]);
-    for (int i = 0; i < args.monitors->count; ++i)
-    {
-        pcap_close(live[i]);
+        case SIGINT:
+        case SIGABRT:
+        case SIGKILL:
+            printf("caught signal.\n");
+            if (live)
+            {
+                pcap_breakloop(live);
+            }
+            return;
     }
 }
 
@@ -258,9 +283,10 @@ int main(int argc, char **argv)
     memset(pcaperr, 0, sizeof(pcaperr));
     parse_args(argc, argv);
     print_device_args();
-    verify_device_names();
+    verify_device_name();
     verify_monitor_mode();
-    listen_for_associations();
+    listen();
+    shutdown(0);
     printf("\n");
     return 0;
 }
